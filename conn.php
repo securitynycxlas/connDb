15b6bb766def83a390622ded08d899e3532f672c
scr1pt <91566916+securitynycxlas@users.noreply.github.com>
2022-01-18 20:35:31 -0300

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "Localhost";
+$database = "databasename";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
